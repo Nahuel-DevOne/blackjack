da591cb40545dd5045d8bf8fcd13445c187dfd5b
Nahuel <nahuel.profesor@gmail.com>
2023-02-23 20:38:06 -0300

    function to get the value of the card, finished

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -48,7 +48,26 @@ const pedirCarta = () => {
     return carta;
 }
 
-pedirCarta();
+// pedirCarta();
 
+const valorCarta = carta => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    // Optimizando el código con ternarios
+    return (isNaN(valor)) ? 
+        (valor === 'A') ? 11 : 10 
+        : valor * 1;
+    
+    // let puntos = 0;
+    // if ( isNaN(valor) ) {        
+    //     puntos = (valor === 'A') ? 11 : 10;
+    // } else {
+    //     // puntos = Number(valor);
+    //     puntos = valor * 1;
+    // }
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({valor});
 
 
